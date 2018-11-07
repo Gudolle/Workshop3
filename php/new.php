ac3d290c0d6d8fb0eb6916549ebf0bcbfd451c94
stevenribette <steven.ribette@epsi.fr>
2018-11-07 13:19:14 +0100

    php

--- php/new.php
+++ php/new.php
@@ -21,6 +21,23 @@ $autre_profession;
 $anciennete_salary;
 
 $temps_non_salary;
+$exerce_activite;
+$seul;
+$combien_salary;
+$poste;
+$administratif;
+$manuel;
+$deplacement;
+
+$prestation;
+$date_accident;
+$rechute;
+$date_rechute;
+$hospitalise;
+$debut_hospitalise;
+$fin_hospitalise;
+$reprise;
+$date_reprise;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["type_contrat"])) {
@@ -137,7 +154,100 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     }
                 }
             }
+            if (empty($_POST["exerce_activite"])) {
+                array_push($nameErr,"Il est obligatoire de nous préciser depuis combien de temps vous exercer votre/vos activités.");
+            }else{
+                $exerce_activite = check($_POST["exerce_activite"]);
+            }
+            if (empty($_POST["seul"])) {
+                array_push($nameErr,"Il est obligatoire de nous préciser si vous travaillez seul.");
+            }else{
+                $seul = check($_POST["seul"]);
+                if($_POST["temps_salary"]=="partiel"){
+                    if (empty($_POST["combien_salary"])) {
+                        array_push($nameErr,"Il est obligatoire de nous préciser combien vous avez de salarié.");
+                    }else{
+                        $combien_salary = check($_POST["combien_salary"]);
+                    }
+                    if (empty($_POST["poste"])) {
+                        array_push($nameErr,"Il est obligatoire de nous préciser à quel(s) poste(s) sont affecté(s) vos salarié.");
+                    }else{
+                        $poste = check($_POST["poste"]);
+                    }
+                }
+            }
+            if (empty($_POST["administratif"])) {
+                array_push($nameErr,"Il est obligatoire de nous préciser la part en % de travail administratif dans votre activité.");
+            }else{
+                $administratif = check($_POST["administratif"]);
+            }
+            if (empty($_POST["manuel"])) {
+                array_push($nameErr,"Il est obligatoire de nous préciser la part en % de travail manuel dans votre activité.");
+            }else{
+                $manuel = check($_POST["manuel"]);
+            }
+            if (empty($_POST["deplacement"])) {
+                array_push($nameErr,"Il est obligatoire de nous préciser la part en % de déplacement dans votre activité.");
+            }else{
+                $deplacement = check($_POST["deplacement"]);
+            }
         }
+    }
+    if (empty($_POST["prestation"])) {
+        array_push($nameErr,"Il est obligatoire de nous préciser la cause de cette prestation.");
+    }else{
+        $prestation = check($_POST["prestation"]);
+    }
+    if (empty($_POST["date_accident"])) {
+        array_push($nameErr,"Il est obligatoire de nous préciser la date de l'accident ou de déclaration de la maladie.");
+    }else{
+        $date_accident = check($_POST["date_accident"]);
+    }
+    if (empty($_POST["rechute"])) {
+        array_push($nameErr,"Il est obligatoire de nous préciser s'il s'agit d'une rechute.");
+    }else{
+        $rechute = check($_POST["rechute"]);
+        if($_POST["rechute"]=="true"){
+            if (empty($_POST["date_rechute"])) {
+                array_push($nameErr,"Il est obligatoire de nous préciser la date de la première chute.");
+            }else{
+                $date_rechute = check($_POST["date_rechute"]);
+            }
+        }
+    }
+    if (empty($_POST["hospitalise"])) {
+        array_push($nameErr,"Il est obligatoire de nous préciser si vous avez était hospitalisé.");
+    }else{
+        $hospitalise = check($_POST["hospitalise"]);
+        if($_POST["hospitalise"]=="true"){
+            if (empty($_POST["debut_hospitalise"])) {
+                array_push($nameErr,"Il est obligatoire de nous préciser la date de début de l'hospitalisation.");
+            }else{
+                $debut_hospitalise = check($_POST["debut_hospitalise"]);
+            }
+            if (empty($_POST["fin_hospitalise"])) {
+                array_push($nameErr,"Il est obligatoire de nous préciser la date de fin de l'hospitalisation.");
+            }else{
+                $fin_hospitalise = check($_POST["fin_hospitalise"]);
+            }
+        }
+    }
+    if (empty($_POST["reprise"])) {
+        array_push($nameErr,"Il est obligatoire de nous préciser si vous avez repris le travail.");
+    }else{
+        $reprise = check($_POST["reprise"]);
+        if($_POST["reprise"]=="true"){
+            if (empty($_POST["date_reprise"])) {
+                array_push($nameErr,"Il est obligatoire de nous préciser la date de votre retour à fonction.");
+            }else{
+                $date_reprise = check($_POST["date_reprise"]);
+            }
+        }
+    }
+    if (empty($_POST["souscrit"])) {
+        array_push($nameErr,"Il est obligatoire de nous préciser si vous avez souscris à d'autres contrat auprès d'AXA et/ou d'une autre compagnie.");
+    }else{
+        $souscrit = check($_POST["souscrit"]);
     }
     add_contrat();
 }
